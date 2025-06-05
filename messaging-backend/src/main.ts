8104bfa615f5d6c1b08286841f2cdb9a6a4f62ba
manoj13196 <akaramsettimanojsai13196@gmail.com>
2025-06-05 16:44:49 +0530

    changed url

--- messaging-backend/src/main.ts
+++ messaging-backend/src/main.ts
@@ -1,12 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+   const configService = app.get(ConfigService);
+ const allowedOrigins = configService.get<string>('FRONTEND_URL')?.split(',') || [];
   app.enableCors({
-    origin: 'http://localhost:5173',
-    credentials: true,               
+   origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    credentials: true,             
   });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
